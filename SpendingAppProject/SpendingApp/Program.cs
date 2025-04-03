3f57ae935168eb754aa3362acdcbb1a1f3a32d15
Matthew Cluck <53344005+matthewcluck@users.noreply.github.com>
2025-04-03 17:23:48 +0000

    Added program main

--- SpendingAppProject/SpendingApp/Program.cs
+++ SpendingAppProject/SpendingApp/Program.cs
@@ -1,9 +1,22 @@
 ﻿namespace SpendingApp;
+using System.IO;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int balance;
+        
+        do {
+            Console.WriteLine("Please enter your bank account balance. No dollar sign, round to the nearest dollar.");
+            string input = Console.ReadLine();
+
+            if (!int.TryParse(input, out balance))
+            {
+                    Console.WriteLine("Invalid input. Please enter a whole number.");
+                    continue;
+            }
+        } while (balance != 700);
+
     }
 }
